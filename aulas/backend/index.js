7dda31efd77790d292acc391e8f02a6b508157a4
JonathanLima25 <jonathan.lima.sjc23@gmail.com>
2020-03-23 22:37:53 -0300

    retornando um json na requisição

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    return response.send('Olá, mundo!')
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: "Jonathan Lima"
+    })
 })
 
 app.listen(3000)
